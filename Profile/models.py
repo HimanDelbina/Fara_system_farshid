4a8fbcf285b196827844218440877122c459fdbc
unknown <himandelbina@gmail.com>
2024-11-16 09:52:03 +0330

    add api

--- Profile/models.py
+++ Profile/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class UserModel(models.model):
+class UserModel(models.Model):
 
     user_name = models.CharField(max_length=20,verbose_name="user_name")
     email = models.EmailField(max_length=254,verbose_name="email")
--- Profile/views.py
+++ Profile/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
 
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def get_all_user(request):
+    data = UserModel.objects.all()
+    return Response(UserSerializers(data, many=True).data, status=status.HTTP_200_OK)
